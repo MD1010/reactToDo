0c9994a8c0362066e03d74777065356208d49e0c
michael <michaelkatom10@gmail.com>
2019-02-02 21:04:43 +0200

    added fields to signup form

--- react-to-do/src/components/Authentication/SignUp.js
+++ react-to-do/src/components/Authentication/SignUp.js
@@ -10,14 +10,18 @@ class SignUp extends Component {
         this.state =
         {
             userName: "",
+            firstName: "",
+            lastName: "",
             password: "",
             email: "",
+            retypedPassword: "",
             submitted: false,
         
             formErrors: {
                 userName: "",
                 email: "",
-                password: ""
+                password: "",
+                retypedPassword: ""
             }
         }
     }
@@ -44,15 +48,19 @@ class SignUp extends Component {
                       this.setState({formErrors:{userName:"username already taken"}})
                    else{
                         this.setState({
+                            firstName: "",
+                            lastName: "",
                             userName: "",
                             password: "",
+                            retypedPassword: "",
                             email: "",
                             submitted: false,
             
                             formErrors: {
                                 userName: "",
                                 email: "",
-                                password: ""
+                                password: "",
+                                retypedPassword: ""
                             }
                         })
                     }
@@ -69,24 +77,28 @@ class SignUp extends Component {
    
     validateFormFields = async () => 
     {
-        let { userName, password, email } = this.state
+        let { userName, password, email, retypedPassword } = this.state
         let valid = true
         const spaceRegex = RegExp(/\s/)
         const passwordRegex = RegExp(/^.{4,8}$/)
         const emailRegex = RegExp(
             /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
             );
-        let userErrorMessage = userName.trim().length >= 4 ? "": "userName has to be at least 4 characters"
-        if(userErrorMessage === "")
+            let userErrorMessage = userName.trim().length >= 4 ? "": "userName has to be at least 4 characters"
+            if(userErrorMessage === "")
             userErrorMessage = spaceRegex.test(userName) ? "username cannot have whitespaces" : ""
-        let passwordErrorMessage = passwordRegex.test(password) ? "": "password has to be between 4 and 8 characters" 
-        let emailErrorMessage = emailRegex.test(email) ? "" : "Invalid Email address"
-          
-        await this.setState({
+            let passwordErrorMessage = passwordRegex.test(password) ? "": "password has to be between 4 and 8 characters" 
+            let emailErrorMessage = emailRegex.test(email) ? "" : "Invalid Email address"
+            let retypedPasswordMessage = ""
+            if(passwordErrorMessage.length === 0 && retypedPassword) {
+                retypedPasswordMessage = password == retypedPassword ? "" : "passwords don't match"
+            }  
+            await this.setState({
             formErrors : {
                 userName : userErrorMessage,
                 email : emailErrorMessage,
-                password : passwordErrorMessage
+                password : passwordErrorMessage,
+                retypedPassword : retypedPasswordMessage
             }
         })
 
@@ -112,6 +124,12 @@ class SignUp extends Component {
                 <div className="transparent-bg sign-up"></div>
                 <form className="form" onSubmit={this.submitForm}>
                     <div className="title">Sign Up</div>
+
+                    <div className="fullName">
+                        <input className="firstName field" spellCheck="false" autoComplete="off" placeholder="firstName" type="text" id="firstName" value={this.state.firstName} onChange={this.handleChange} maxLength="13"></input>
+                        <input className="lastName field" spellCheck="false" autoComplete="off"  placeholder="lastName" type="text" id="lastName" value={this.state.lastName} onChange={this.handleChange} maxLength="13"></input>
+                    </div>
+                    
                     <input className="username field" spellCheck="false" autoComplete="off" className={formErrors.userName.length > 0 ? "error" : "input-bar"}  placeholder="username" type="text" id="userName" value={this.state.userName} onChange={this.handleChange} maxLength="13"></input>
                     <label className="error-label">{formErrors.userName}</label>  
                     
@@ -120,6 +138,9 @@ class SignUp extends Component {
                     
                     <input className="password field" spellCheck="false" autoComplete="off" className={formErrors.password.length > 0 ? "error" : "input-bar"} placeholder="password" type="password" id="password" value={this.state.password} onChange={this.handleChange} maxLength="13"></input>
                     <label className="error-label">{formErrors.password}</label>
+                    
+                    <input className="retypedPassword field" spellCheck="false" autoComplete="off" className={formErrors.retypedPassword.length > 0 ? "error" : "input-bar"} placeholder="retype password" type="password" id="retypedPassword" value={this.state.retypedPassword} onChange={this.handleChange} maxLength="13"></input>
+                    <label className="error-label">{formErrors.retypedPassword}</label>
                     
                     <button type="submit" id="submit-button" >Create Account</button>     
                 </form>
